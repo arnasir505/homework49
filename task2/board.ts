cb6cbb15bb767916f553a035019763f927ef1819
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2023-12-15 15:02:46 +0600

    Added main functionality to boart.ts

--- task2/board.ts
+++ task2/board.ts
@@ -1,1 +1,14 @@
-console.log('Hello world')
+const whiteSquare = '  ';
+const blackSquare = '██';
+let chessBoard = '';
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 === 0) {
+            chessBoard += whiteSquare;
+        } else {
+            chessBoard += blackSquare;
+        }
+    }
+    chessBoard += '\n';
+}
+console.log(chessBoard);
